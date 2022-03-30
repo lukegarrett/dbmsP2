182e04725870c548b5ff2d47082b03ddd6dec76e
mwdobson <michael.w.dobson@vanderbilt.edu>
2022-03-30 14:16:35 -0500

    ''

---
+++ project_2_load_data.sql
@@ -0,0 +1,122 @@
+-- Michael Dobson and Luke Garrett --
+-- Vanderbilt University --
+-- CS 3265: Database Management Systems --
+-- US Accidents Script File --
+-- LOAD DATA SCRIPT --
+
+-- Create database --
+DROP DATABASE IF EXISTS us_accidents;
+CREATE DATABASE us_accidents;
+USE us_accidents;
+
+-- Create megatable --
+DROP TABLE IF EXISTS accidents_megatable;
+CREATE TABLE accidents_megatable (
+	accident_id VARCHAR(20) NOT NULL,
+    severity TINYINT,
+    start_time DATETIME,
+    end_time DATETIME,
+    start_lat DECIMAL,
+    start_lng DECIMAL,
+    end_lat DECIMAL,
+    end_lng DECIMAL,
+    distance INT,
+    description VARCHAR(100),
+    street_number INT,
+    street VARCHAR(20),
+    side VARCHAR(1),
+    city VARCHAR(15),
+    county VARCHAR(15),
+    state VARCHAR(2),
+    zipcode VARCHAR(5),
+    country VARCHAR(2),
+    timezone VARCHAR(15),
+    airport_code VARCHAR(4),
+    weather_timestamp DATETIME,
+    temperature INT,
+    wind_chill INT,
+    humidity DECIMAL,
+    pressure DECIMAL,
+    visibility DECIMAL,
+    wind_direction VARCHAR(4),
+    wind_speed INT,
+    precipitation DECIMAL,
+    weather_condition VARCHAR(10),
+    amenity VARCHAR(5),
+    bump VARCHAR(5),
+    crossing VARCHAR(5),
+    give_way VARCHAR(5),
+    junction VARCHAR(5),
+    no_exit VARCHAR(5),
+    railway VARCHAR(5),
+    roundabout VARCHAR(5),
+    station VARCHAR(5),
+    stop VARCHAR(5),
+    traffic_calming VARCHAR(5),
+    traffic_signal VARCHAR(5),
+    turning_loop VARCHAR(5),
+    sunrise_sunset VARCHAR(5),
+    civil_twilight VARCHAR(5),
+    nautical_twilight VARCHAR(5),
+    astronomical_twilight VARCHAR(5),
+    PRIMARY KEY (accident_id)    
+) ENGINE = INNODB;
+
+INSERT INTO accidents_megatable
+VALUES ('A-1',
+		3,
+        '2016-02-08 00:37:08',
+        '2016-02-08 06:37:08', 
+        40.108909999999995,
+        -83.09286,
+        40.11206,
+        -83.03187,
+        3.23,
+        'Between Sawmill Rd/Exit 20 and OH-315/Olentangy Riv Rd/Exit 22 - Accident.',
+        0,
+        'Outerbelt E',
+        'R',
+        'Dublin',
+        'Franklin',
+        'OH',
+        '43017',
+        'US',
+        'US/Eastern',
+        'KOSU',
+        '2016-02-08 00:53:00',
+        42.1,
+        36.1,
+        58.0,
+        29.76,
+        10.0,
+        'SW',
+        10.4,
+        0.0,
+        'Light Rain',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'False',
+        'Night',
+        'Night',
+        'Night',
+        'Night');
+
+SELECT COUNT(*)
+FROM accidents_megatable;
+
+LOAD DATA
+	LOCAL INFILE 'C:/Users/micha/Downloads/US_Accidents_Dec21_updated.csv' 
+	INTO TABLE accidents_megatable 
+	FIELDS TERMINATED BY ',' 
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n';
